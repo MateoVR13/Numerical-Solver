2efea3b78d2de41391e1845ac9aec5c51f347397
MateoVR13 <mattvr130@gmail.com>
2024-11-14 14:19:53 -0500

    Updated __init__.py files

---
+++ ui/__init__.py
@@ -0,0 +1,9 @@
+from . import *
+
+"""
+UI module initialization for Numerical Analysis Project.
+This package contains user interface components and related functionality.
+"""
+
+
+__version__ = '1.0.0'
