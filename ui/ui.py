551d7d87a58a9185b431ced2181999033a8b0ffd
MateoVR13 <mattvr130@gmail.com>
2024-11-17 07:16:02 -0500

    Modularization of bisection

--- ui/ui.py
+++ ui/ui.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import sympy as sp
 import math
 from pprint import pprint
+from methods.bisection import metodo_biseccion
 
 st.set_page_config(layout="wide", page_title="Solucionador Numérico", page_icon = "⚛️")
 
@@ -16,40 +17,40 @@ tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8 = st.tabs([
 
 # ----------------------------------- BISECTION TAB -------------------------------------------------
 
-def metodo_biseccion(a, b, funcion, error_minimo = None, max_iteraciones = None):
-    def evaluar_funcion(x):
-        return eval(funcion)
+# def metodo_biseccion(a, b, funcion, error_minimo = None, max_iteraciones = None):
+#     def evaluar_funcion(x):
+#         return eval(funcion)
 
-    c = (a + b) / 2
-    est_error = 100
-    list_vals = [a, b, c, est_error]
-    list_its = [list_vals]
-    new_itr = 0
+#     c = (a + b) / 2
+#     est_error = 100
+#     list_vals = [a, b, c, est_error]
+#     list_its = [list_vals]
+#     new_itr = 0
 
-    while True:
-        new_list = [evaluar_funcion(x) for x in list_vals[:3]]
+#     while True:
+#         new_list = [evaluar_funcion(x) for x in list_vals[:3]]
 
-        if (new_list[0] * new_list[2]) < 0:
-            list_vals = [list_vals[0], list_vals[2]]
-        elif (new_list[0] * new_list[2]) > 0:
-            list_vals = [list_vals[2], list_vals[1]]
-        else:
-            break
+#         if (new_list[0] * new_list[2]) < 0:
+#             list_vals = [list_vals[0], list_vals[2]]
+#         elif (new_list[0] * new_list[2]) > 0:
+#             list_vals = [list_vals[2], list_vals[1]]
+#         else:
+#             break
 
-        list_vals.append((list_vals[0] + list_vals[1]) / 2)
-        new_c = list_vals[2]
-        old_c = list_its[new_itr][2]
-        est_error = abs((new_c - old_c) / new_c) * 100
-        list_vals.append(est_error)
-        list_its.append(list_vals)
-        new_itr += 1
+#         list_vals.append((list_vals[0] + list_vals[1]) / 2)
+#         new_c = list_vals[2]
+#         old_c = list_its[new_itr][2]
+#         est_error = abs((new_c - old_c) / new_c) * 100
+#         list_vals.append(est_error)
+#         list_its.append(list_vals)
+#         new_itr += 1
 
-        if error_minimo is not None and est_error <= error_minimo:
-            break
-        if max_iteraciones is not None and new_itr >= max_iteraciones:
-            break
+#         if error_minimo is not None and est_error <= error_minimo:
+#             break
+#         if max_iteraciones is not None and new_itr >= max_iteraciones:
+#             break
 
-    return new_itr + 1, list_vals[2], est_error, list_its
+#     return new_itr + 1, list_vals[2], est_error, list_its
 
 with tab1:
     col1, col2,col3 = st.columns([1, 1, 1])
